577614910136df70f19019237ba26d9a02029da6
msmithmlrnetworks <msmith@mlrnetworks.co.uk>
2022-07-12 13:43:54 +0100

    updates

--- app/datascraper.py
+++ app/datascraper.py
@@ -1,22 +1,8 @@
-# from __future__ import (
-#     absolute_import,
-#     division,
-#     print_function,
-#     unicode_literals,
-# )
-# from builtins import *
-
-
 import csv
+from re import M
 import time
 import pandas as pd
 import netmiko
-
-
-# netmiko_excpetions = (netmiko.ssh_exception.NetMikoTimeoutException,
-#                       netmiko.ssh_exception.NetMikoAuthenticationException,
-#                       netmiko.ssh_exception.SSHException)
-
 
 reader = csv.DictReader(open('app/switches.csv', 'r'))
 for row in reader:
@@ -35,18 +21,17 @@ for row in reader:
         device_type=device_type, username=username,\
             password=password)
         mac_table = connection.send_command(command, use_textfsm=True)
-        mac_data = {'device hostname': hostname,'mac':  [entry['mac'] for entry in mac_table],\
-        'interface': [entry['ports'] for entry in mac_table],\
+        mac_data = {'mac':  [entry['destination_address'] for entry in mac_table],\
+        'interface': [entry['destination_port'] for entry in mac_table],\
         'vlan': [entry['vlan'] for entry in mac_table]}
-        print(mac_data)
         connection.disconnect()
         df = pd.DataFrame(mac_data, columns=list(mac_data.keys()))
-        writer = pd.ExcelWriter('app/data/mac_table.xlsx', engine='xlsxwriter')
+        writer = pd.ExcelWriter('data/mac_table.xlsx', engine='xlsxwriter')
         df.to_excel(writer, hostname)
         writer.save()
-        print(df)
         success = True
 
+        time.sleep(102)
     except:
         success = False
         time.sleep(2)
