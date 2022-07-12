b9c6cd03b4785b3fcc03c075f4c99cec831795c5
msmithmlrnetworks <msmith@mlrnetworks.co.uk>
2022-07-12 15:39:06 +0100

    a

--- app/datascraper.py
+++ app/datascraper.py
@@ -27,7 +27,7 @@ for row in reader:
         connection.disconnect()
         print(connection.is_alive())
         df = pd.DataFrame(mac_data, columns=list(mac_data.keys()))
-        writer = pd.ExcelWriter('/app/data/mac_table.xlsx', engine='xlsxwriter',mode='a', if_sheet_exists='overlay')
+        writer = pd.ExcelWriter('/app/data/mac_table.xlsx', engine='xlsxwriter',mode='a')
         df.to_excel(writer, hostname)
         writer.save()
         success = True
